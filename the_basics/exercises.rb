81f205854572a1100751412d4f0c9ffb8c7b3e58
Laurent Sercomanens <Laurent.Sercomanens@gmail.com>
2023-07-19 12:45:01 +0200

    Do the exercises

---
+++ the_basics/exercises.rb
@@ -0,0 +1,48 @@
+puts "John " + "Mallone"
+puts
+puts 4726 / 1000
+puts 4726 / 100 % 8
+puts 4726 % 4
+puts 4726.remainder(8)
+puts
+
+# puts {:The_wild_goat => 1975, :Pizza_Apolcalypse => 2004, :Bonkers_Office => 2013, :The_Mad_scientist => 2001, :Miss_Mellow_Yellow => 1981}[:The_wild_goat] 
+# above code did not run
+
+movies = {  :the_wild_goat => 1975,
+            :pizza_apolcalypse => 2004,
+            :bonkers_office => 2013,
+            :the_mad_scientist => 2001,
+            :miss_mellow_yellow => 1981}
+
+puts movies[:the_wild_goat]
+puts movies[:pizza_apolcalypse]
+puts movies[:bonkers_office]
+puts movies[:the_mad_scientist]
+puts movies[:miss_mellow_yellow]
+
+puts
+
+dates = [1975,2004,2013,2001,1981]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+puts
+
+# puts 5 ** 5
+puts 5 * 4 * 3 * 2 * 1
+# puts 6 ** 6
+puts 6 * 5 * 4 * 3 * 2 * 1
+# puts 7 ** 7
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+# puts 8 ** 8
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts
+
+puts 3.24 ** 2
+puts 68.7 ** 2
+puts 12.876 ** 2
---
+++ the_basics/strings.rb
@@ -0,0 +1,7 @@
+puts 'the man said Blah Blah'
+puts "the man said Blah Blah"
+#  puts `the man said Blah Blah`       back ticks do not work
+#  puts "the man said 'Blah Blah'"     also does not work
+#  puts 'the man said \'Blah Blah\''   also does not work
+#  puts 'the man said \'Blah Blah'\'   also does not work
+puts "the man said \'Blah Blah\'"   # What works is double quotes and single quites ignored
