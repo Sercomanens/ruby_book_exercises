a3de9c3747e88a516163e6c25c1b2787855be23e
Laurent Sercomanens <Laurent.Sercomanens@gmail.com>
2023-07-24 10:00:49 +0200

    Do arrays exercises

---
+++ arrays/exercises.rb
@@ -0,0 +1,13 @@
+# r = [1,2,3,4,5,6,7,8,9]
+# num = 6
+# if r.include?(num)
+#   puts "#{num} is included in the array!"
+# end
+
+arr = [1,2,3,4,5,6,7,8,9]
+arr.each do |number|
+  puts arr
+  puts "#{number + 2}"
+end
+
+#Not really there yet!
